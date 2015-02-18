dad1825daecbeef76c21d816a591fed51dfa1612
Nick Mancini <nmancini11@gmail.com>
2015-02-17 20:04:09 -0800

    clean up clean up clean up

--- js/application.js
+++ js/application.js
@@ -1,29 +1,45 @@
 $(document).ready(function () {
-    $('.intro').delay(3000).fadeOut(200);
+    $('.intro').delay(4000).fadeOut(200);
 
-    var $headerIntro = $('.intro');
+    /* Resize background depending on screen width*/
+    function vidHeight(){
+      var winWidth = $( document ).width(),
+          vHeight = winWidth*0.57;
+          if ($(window).width() < 768){
+            vHeight = 330;
+          }
+      $('.cd-fixed-bg').css("min-height", vHeight);
+      $('.video').css("height", vHeight);
+    }
+    vidHeight();
 
-    //open-close submenu on mobile
-    $('.cd-main-nav').on('click', function (event) {
-        if ($(event.target).is('.cd-main-nav')) { 
-            $(this).children('ul').toggleClass('is-visible');
-        }
+
+    $('.cd-main-nav').on('click', function(event){
+      if($(event.target).is('.cd-main-nav')){
+        $(this).children('ul').toggleClass('is-visible').slideDown(500);
+      }
     });
 
-    //hide header text which are outside the viewport
-    $headerIntro.each(function () {
-        if ($(this).offset().top > $(window).scrollTop() + $(window).height() * 0.75) {
-            $(this).addClass('is-hidden');
-        }
+    $('.cd-main-nav ul li a').on('click', function(){
+      if ($(window).width() < 768){
+        $(this).closest('ul').toggleClass('is-visible').fadeOut(1000);
+      }
     });
 
-    $(window).on('scroll', function () {
-        $headerIntro.each(function () {
-            if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.75 && $(this).hasClass('is-hidden')) {
-                $(this).removeClass('is-hidden').addClass('bounce-in');
-            }
-        });
+
+
+    // Animate clocks on scroll using Waypoint.js & Inview
+    var inview = new Waypoint.Inview({
+      element: $('.clock'),
+      entered: function() {
+        $('.clock').fadeIn(2000).circliful().addClass("animated pulse");
+      },
+      exit: function() {
+        // calling inview.destroy prevent multiple clocks from showing on the page
+        inview.destroy();
+      },
     });
+
 
     /* Add Scroll To Effect */
     $(function () {
@@ -33,7 +49,7 @@ $(document).ready(function () {
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
                     $('html,body').animate({
-                        scrollTop: target.offset().top
+                        scrollTop: target.offset().top-50
                     }, 1000);
                     return false;
                 }
@@ -41,30 +57,35 @@ $(document).ready(function () {
         });
     });
 
-    /* Having fun with some clock animation */
-    var seconds = 10;
-    var doPlay = true;
-    var loader = document.getElementById('loader'),
-        α = 0,
-        π = Math.PI,
-        t = (seconds/360 * 1000);
+    // Resize video container on resizing of Window
+    $(window).on("resize", vidHeight);
 
-    $(function draw() {
-      α++;
-      α %= 360;
-      var r = ( α * π / 180 ),
-         x = Math.sin( r ) * 125,
-         y = Math.cos( r ) * - 125,
-         mid = ( α > 180 ) ? 1 : 0,
-         anim = 'M 0 0 v -125 A 125 125 1 ' + mid + ' 1 ' +  x  + ' ' +  y  + ' z';
-      //[x,y].forEach(function( d ){
-      //  d = Math.round( d * 1e3 ) / 1e3;
-      //});
-     
-      loader.setAttribute( 'd', anim );
-      
-      if(doPlay){
-        setTimeout(draw, t); // Redraw
+    if ($(window).width() < 768){
+      $('#social').css('background-color','#000');
+    }
+
+
+    // *** Fade In Effect ***
+    var element = document.getElementById("js-fadeInElement");
+    $(element).addClass('js-fade-element-hide');
+
+    $(window).scroll(function() {
+      if( $("#js-fadeInElement").length > 0 ) {
+        var elementTopToPageTop = $(element).offset().top;
+        var windowTopToPageTop = $(window).scrollTop();
+        var windowInnerHeight = window.innerHeight;
+        var elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
+        var elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
+        var distanceFromBottomToAppear = 400;
+
+        if(elementTopToWindowBottom > distanceFromBottomToAppear) {
+          $(element).addClass('js-fade-element-show');
+        }
+        else if(elementTopToWindowBottom < 0) {
+          $(element).removeClass('js-fade-element-show');
+          $(element).addClass('js-fade-element-hide');
+        }
       }
     });
+
 });
